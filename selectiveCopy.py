c65e634c3cab1b16d5056db59484d2d75b9d8718
Josh <j.j.h.lindner@gmail.com>
2021-06-13 16:03:49 -0700

    Debug and add fixes. Add check for existing folder.

--- selectiveCopy.py
+++ selectiveCopy.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
 # Usage
-# selectiveCopy.py <folder> <extension>
+# selectiveCopy.py <folder path> <file extension>
 
 import os
+import shutil
 import sys
+from shutil import copyfile
 
 def selectiveCopy(folder, fileExt):
-    # Create new folder
+    # Create new folder if it doesn't already exist
     ext = fileExt.replace(".","")
-    os.mkdir(folder+"\\{}_files".format(ext))
+    newFolder=folder+"\\{}_files".format(ext)
+    if not os.path.isdir(newFolder):
+        os.mkdir(newFolder)
     #  Walk through folder, check extension
-    for folder, subfolder, file in os.walk(os.path(folder)):
-        for filename in file:
+    for folders, subfolders, files in os.walk(folder):
+        for filename in files:
             if filename.endswith(fileExt):
-                # Move specific files to new folder
-                os.copy(os.path.abspath(filename),folder+"\\{}_files\\{}".format(fileExt,filename) )
+                # Copy specific files to new folder
+                shutil.copyfile(folder+"\\"+filename,newFolder+"\\"+filename )
 
 
 
@@ -22,5 +26,5 @@ def selectiveCopy(folder, fileExt):
 if __name__ == "__main__":
     folder = sys.argv[1]
     fileExt = sys.argv[2]
-
+    
     selectiveCopy(folder,fileExt)
